3488ad1eac1dd6a2076a02450f1169547fad05b0
Vít Kilián <kilianv@spseplzen.cz>
2021-04-07 22:13:50 +0200

    formular upraven

--- src/Components/ImageLinkForm/ImageLinkForm.js
+++ src/Components/ImageLinkForm/ImageLinkForm.js
@@ -5,11 +5,11 @@ const ImageLinkForm = ({ onInputChange,  onButtonSubmit }) => {
     return(
         <div>
             <p className='f3'>
-                {'This Magin Brain will detect faces in your pictures. Give it a way'}
+                {'This Magic Brain will detect faces in your pictures. Give it a URL'}
             </p>
             <div className='center'>
                 <div className='form center pa4 br3 shadow-5'>
-                    <input className='f4 pa2 w-70 center' type='text' onChange={onInputChange}/>
+                    <input className='f4 pa2 w-70 center' type='text' onChange={onInputChange} placeholder="Put in URL"/>
                     <button className='w-30 grow f4 link ph3 pv2 dib bg-light-blue'
                     onClick={onButtonSubmit}
                     >Detect</button>
